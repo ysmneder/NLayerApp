51f2478b9613322ab346785fc9582f7fce3d2fee
yasemin <ysmneder@gmail.com>
2023-06-06 16:02:40 +0300

    Global Exception Handler

---
+++ App.Api/Middlewares/UseCustomExceptionHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using App.Services.Exceptions;
+using App.Core.Dtos;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Text.Json;
+
+namespace App.Api.Middlewares
+{
+    public static class UseCustomExceptionHandler
+    {
+        public static void UseCustomException(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+
+                    var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    var statusCode = exceptionFeature.Error switch
+                    {
+                        ClientSideException => 400,
+                        NotFoundException => 404,
+                        _ => 500
+                    };
+
+                    context.Response.StatusCode = statusCode;
+
+                    var response = CustomResponseDto<NoContentDto>.Fail(statusCode, exceptionFeature.Error.Message);
+
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+
+                });
+
+            });
+        }
+    }
+}
+
